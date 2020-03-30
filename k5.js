8ed2543140a11234d30f15d52a606f2d7a17e5a3
Darian0202 <61963957+Darian0202@users.noreply.github.com>
2020-03-30 08:49:34 +0200

    started on javascript, next step is to create a function

---
+++ k5.js
@@ -0,0 +1,4 @@
+const burgerMenuButton = document.querySelector(".burger-menu-button");
+burgerMenuButton.addEventListener("click", toggleBurgerMenu);
+const burgerMenu = document.querySelector(".burger-menu");
+let burgerMenuOpel = false;
