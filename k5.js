1c7a859b5e292e8165243565f1d504d4b07f0875
Darian0202 <61963957+Darian0202@users.noreply.github.com>
2020-04-11 12:06:28 +0200

    Made a button for contact information

--- k5.js
+++ k5.js
@@ -15,3 +15,21 @@ function toggleBurgerMenu()
         burgerMenu.style.display = "none";
     }
 }
+
+const contactButton = document.querySelector(".contact-button");
+contactButton.addEventListener("click", toggleContact);
+const contactMenu = document.querySelector(".contact");
+let contactMenuOpen = false;
+
+function toggleContact()
+{
+    contactMenuOpen = !contactMenuOpen;
+    if (contactMenuOpen) {
+        contactMenu.style.display = "block";
+    } 
+    else
+    {
+        contactMenu.style.display = "none";
+    }
+}
+
